c7edcca68d4b3316e1556e2602b069e77ed56645
Haris131 <kabelku01@gmail.com>
2026-06-12 17:57:53 +0700

    fix: bypass libcap-ng pkg-config check in openvpn configure

--- scripts/build-openvpn.sh
+++ scripts/build-openvpn.sh
@@ -19,13 +19,14 @@ tar xzf openvpn-2.6.12.tar.gz
 cd openvpn-2.6.12
 export CFLAGS="$CFLAGS -I$DEPS_DIR/openssl/include -I$DEPS_DIR/lzo/include"
 export LDFLAGS="$LDFLAGS -L$DEPS_DIR/openssl/lib -L$DEPS_DIR/lzo/lib"
+export LIBCAPNG_CFLAGS=' '
+export LIBCAPNG_LIBS=' '
 ./configure --host="$HOST" CC="$CC" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" \
   --with-crypto-library=openssl \
   --with-ssl="$DEPS_DIR/openssl" \
   --with-lzo="$DEPS_DIR/lzo" \
   --disable-plugin-auth-pam --disable-plugin-down-root \
   --disable-debug --disable-lz4 --disable-dco \
-  ac_cv_header_libcap_ng_h=no \
   --disable-shared --enable-static
 make -j$(nproc)
 $STRIP src/openvpn/openvpn || true
