078ecbd3e6431dacb5e80aa4d0103d0407b64e1e
Haris131 <kabelku01@gmail.com>
2026-06-13 14:40:39 +0700

    fix: use correct oniguruma download URL (GitHub archive instead of release asset)

--- scripts/build-oniguruma.sh
+++ scripts/build-oniguruma.sh
@@ -2,9 +2,9 @@
 
 cd "$GITHUB_WORKSPACE"
 ONIG_VER="6.9.9"
-wget -q "https://github.com/kkos/oniguruma/releases/download/v${ONIG_VER}/oniguruma-${ONIG_VER}.tar.gz"
+wget -q "https://github.com/kkos/oniguruma/archive/refs/tags/v${ONIG_VER}.tar.gz" -O "oniguruma-${ONIG_VER}.tar.gz"
 tar xzf "oniguruma-${ONIG_VER}.tar.gz"
-cd "oniguruma-${ONIG_VER}"
+cd "oniguruma-${ONIG_VER}" 2>/dev/null || cd "oniguruma-v${ONIG_VER}"
 ./configure --host="$HOST" CC="$CC" AR="$AR" RANLIB="$RANLIB" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" \
   --enable-static --disable-shared \
   --prefix="$DEPS_DIR/oniguruma"
