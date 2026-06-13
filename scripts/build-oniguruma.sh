52e3da868f1dd8a605484eac4edff8282e115db9
Haris131 <kabelku01@gmail.com>
2026-06-13 14:48:18 +0700

    fix: correct oniguruma download to use release asset (onig-6.9.9.tar.gz)

--- scripts/build-oniguruma.sh
+++ scripts/build-oniguruma.sh
@@ -2,9 +2,9 @@
 
 cd "$GITHUB_WORKSPACE"
 ONIG_VER="6.9.9"
-wget -q "https://github.com/kkos/oniguruma/archive/refs/tags/v${ONIG_VER}.tar.gz" -O "oniguruma-${ONIG_VER}.tar.gz"
-tar xzf "oniguruma-${ONIG_VER}.tar.gz"
-cd "oniguruma-${ONIG_VER}" 2>/dev/null || cd "oniguruma-v${ONIG_VER}"
+wget -q "https://github.com/kkos/oniguruma/releases/download/v${ONIG_VER}/onig-${ONIG_VER}.tar.gz"
+tar xzf "onig-${ONIG_VER}.tar.gz"
+cd "onig-${ONIG_VER}"
 ./configure --host="$HOST" CC="$CC" AR="$AR" RANLIB="$RANLIB" CFLAGS="$CFLAGS" LDFLAGS="$LDFLAGS" \
   --enable-static --disable-shared \
   --prefix="$DEPS_DIR/oniguruma"
